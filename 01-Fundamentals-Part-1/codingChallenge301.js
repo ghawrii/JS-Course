699acb8be3719cf90c5544295a4bad1d63251952
Ghawrii <zaeemhafizmuhammad@gmail.com>
2023-12-20 14:42:26 +0500

    Completed Fundamentals Part - 1 section entirely

---
+++ 01-Fundamentals-Part-1/codingChallenge301.js
@@ -0,0 +1,26 @@
+// Dataset 1
+let bill = 275;
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+let total = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${total}.`
+);
+
+// Dataset 2
+bill = 40;
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+total = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${total}.`
+);
+
+// Dataset 3
+bill = 430;
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+total = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${total}.`
+);
