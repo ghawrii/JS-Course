c05b7a1e4eeb356b48184044d1d0c616e49cf396
Ghawrii <zaeemhafizmuhammad@gmail.com>
2023-12-20 10:53:47 +0500

    Completed coding challenge # 3 with all three variations

---
+++ 01-Fundamentals-Part-1/codingChallenge201.js
@@ -0,0 +1,25 @@
+// Dolphin Scores
+const dolphinScore1 = 96;
+const dolphinScore2 = 108;
+const dolphinScore3 = 89;
+// Koala Scores
+const koalaScore1 = 88;
+const koalaScore2 = 91;
+const koalaScore3 = 110;
+
+// Sums of scores
+const dolphinSum = dolphinScore1 + dolphinScore2 + dolphinScore3;
+const koalaSum = koalaScore1 + koalaScore2 + koalaScore3;
+
+// Average scores
+const dolphinAvg = dolphinSum / 3;
+const koalaAvg = koalaSum / 3;
+
+// Determining and printing results
+if (koalaAvg > dolphinAvg) {
+  console.log("Koalas win!");
+} else if (dolphinAvg > koalaAvg) {
+  console.log("Dolphins win!");
+} else {
+  console.log("It was a draw 😦");
+}
---
+++ 01-Fundamentals-Part-1/codingChallenge202.js
@@ -0,0 +1,25 @@
+// Dolphin Scores
+const dolphinScore1 = 97;
+const dolphinScore2 = 112;
+const dolphinScore3 = 101;
+// Koala Scores
+const koalaScore1 = 109;
+const koalaScore2 = 95;
+const koalaScore3 = 123;
+
+// Sums of scores
+const dolphinSum = dolphinScore1 + dolphinScore2 + dolphinScore3;
+const koalaSum = koalaScore1 + koalaScore2 + koalaScore3;
+
+// Average scores
+const dolphinAvg = dolphinSum / 3;
+const koalaAvg = koalaSum / 3;
+
+// Determining and printing results
+if (koalaAvg > dolphinAvg) {
+  console.log("Koalas win!");
+} else if (dolphinAvg > koalaAvg) {
+  console.log("Dolphins win!");
+} else {
+  console.log("It was a draw 😦");
+}
